b3b4edfa9b2a23b11e7fba885e6027ca694e2208
Sébastien NICOU <snicou.pro@gmail.com>
2020-12-14 11:17:16 +0100

    MAJ HOME

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <!doctype html>
-<<<<<<< HEAD
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Accueil - Tekkies</title>
-=======
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/css/app.css">
-    <title>Document</title>
->>>>>>> master
+    <title>Accueil - Tekkies</title>
+
 </head>
 <body>
 <h1>HELLO WORLD</h1>
