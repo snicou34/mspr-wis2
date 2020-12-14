ea6a7e8404905fe8ab0eec32ff90c90bed62e7e9
Sébastien NICOU <snicou.pro@gmail.com>
2020-12-14 15:15:27 +0100

    footer_header_maj

--- includes/footer1.php
+++ includes/footer1.php
@@ -1,9 +1,8 @@
 
 <footer class="page-footer font-small teal pt-4">
 
-    <div class="footer-copyright text-center py-3" style="background-color: #ddddde;">© <?php echo date("Y"); ?>
-        Copyright:
-        <a href="http://localhost/mspr1-wis2/index.php" style="color: inherit; ">Tekkies</a>
+    <div class="footer-copyright text-center py-3" style="background-color: #ddddde;"><i class="fas fa-copyright" style="margin-right: 4px"></i><?php echo date("Y"); ?>
+        by <span class="fw-bold">Tekkies</span>
     </div>
 
 
--- includes/header.php
+++ includes/header.php
@@ -6,27 +6,28 @@
     <title>Accueil - Tekkies</title>
     <link rel="stylesheet" href="assets/css/app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" crossorigin="anonymous">
-    <link rel="icon" type="image/png" href="assets/img/tekkies.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png" sizes="16x16">
 </head>
 
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-3 mb-5 bg-white rounded fixed-top">
     <div class="container-fluid">
-        <a href="index.php" style="margin-right: 6px">
+        <a href="index.php" style="margin-right: 15px">
             <img src="assets/img/tekkies.png" alt="Ceci est un logo" title="Logo Tekkies" class="logo" height="50px">
         </a>
-        <a class="navbar-brand" href="index.php">Tekkies</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+                    <a class="nav-link active fw-bold" aria-current="page" href="index.php"><i class="fas fa-home" style="margin-right: 4px"></i>Accueil</a>
                 </li>
-                <li class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Rechercher sur Tekkies" aria-label="Search">
-                    <button class="btn btn-outline-danger" type="submit">Rechercher</button>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php"><i class="fas fa-users" style="margin-right: 4px"></i>Qui sommes-nous ?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php"><i class="fas fa-bell" style="margin-right: 4px"></i>Notifications</a>
                 </li>
             </ul>
             <li class="navbar-nav ml-auto">
@@ -39,7 +40,11 @@
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="#">
                             <i class="fas fa-user" style="margin-right: 3px"></i>
-                            <span>Voir mon profil</span>
+                            <span>Mon profil</span>
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-question-circle" style="margin-right: 3px"></i>
+                            <span>Aide</span>
                         </a>
                         <hr class="dropdown-divider">
                         <a class="dropdown-item" style="color: red" href="#"><i class="fas fa-sign-out-alt" style="margin-right: 5px"></i>Se déconnecter</a>
