7227496bb8758d2d595c2207ccd8784bbe42945f
Sébastien NICOU <snicou.pro@gmail.com>
2020-12-14 14:37:57 +0100

    header_maj1

--- includes/header.php
+++ includes/header.php
@@ -5,48 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil - Tekkies</title>
     <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="assets/img/tekkies.png" sizes="16x16">
 </head>
 
 <body>
-<header class="col-md-26 col-lg-12 navbar-light bg-white fixed-top shadow-sm" id="header">
-    <nav class="navbar navbar-expand-lg">
-        <a href="index.php" class="mr-4">
-            <img src="assets/img/tekkies.png" alt="Ceci est un logo" title="Logo Tekkies" style="height: 50px; margin-left: 8px">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a href="index.php" style="margin-right: 6px">
+            <img src="assets/img/tekkies.png" alt="Ceci est un logo" title="Logo Tekkies" class="logo" height="50px">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="index.php">Tekkies</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link font-weight-bold" href="./index.php"><i class="fas fa-home mr-2"></i>Accueil</span></a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
                 </li>
-                <li class="nav-item active">
-                    <a class="nav-link font-weight-bold" href="./career.php"><i class="fas fa-address-card mr-2"></i>Mon parcours</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link font-weight-bold" href="./education.php"><i class="fas fa-university mr-2"></i>Ma formation</a>
-                </li>
-                <li class="nav-item active font-weight-bold">
-                    <a class="nav-link" href="./projects.php"><i class="fas fa-layer-group mr-2"></i>Mes projets</a>
-                </li>
-                <li class="nav-item active font-weight-bold">
-                    <a class="nav-link" href="./contact.php"><i class="fas fa-handshake mr-2"></i>Me contacter</a>
+                <li class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Rechercher sur Tekkies" aria-label="Search">
+                    <button class="btn btn-outline-danger" type="submit">Rechercher</button>
                 </li>
             </ul>
-            <ul class="navbar-nav ml-auto">
+            <li class="navbar-nav ml-auto">
                 <div class="btn-group">
-                    <button type="button" class="btn rounded-pill dropdown-toggle" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false" style="background-color: #1E1E1E; color: #FFFFFF">
-                        <i class="fas fa-globe mr-2" style="color: #FFFFFF"></i>Victor Grollier
+                    <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false" style="background-color: #E7383C; color: #FFFFFF">
+                        <img src="assets/img/profile.png" height="30px" style="margin-right: 5px">
+                        Sébastien NICOU
                     </button>
-                    <div class="dropdown-menu rounded-pill">
-                        <a class="dropdown-item" href="#">Mon profil</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">
+                            <i class="fas fa-user" style="margin-right: 3px"></i>
+                            <span>Voir mon profil</span>
+                        </a>
+                        <hr class="dropdown-divider">
+                        <a class="dropdown-item" style="color: red" href="#"><i class="fas fa-sign-out-alt" style="margin-right: 5px"></i>Se déconnecter</a>
                     </div>
                 </div>
-            </ul>
+            </li>
         </div>
-    </nav>
-</header>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php include_once 'includes/header.php'; ?>
-<h1 class="py-5">Hello World</h1>
 
+
+
+
+
+
+<script src="assets/js/manifest.js"></script>
+<script src="assets/js/vendor.js"></script>
 <script src="assets/js/app.js"></script>
+</body>
+</html>
