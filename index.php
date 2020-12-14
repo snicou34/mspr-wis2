17dfd730f85d2f408d7e66ea5788a2bc3e4518e8
Victor Grollier <victor.grollier@gmai.com>
2020-12-14 14:55:40 +0100

    content accueil

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php include_once 'includes/header.php'; ?>
-
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
 
 
 <script src="assets/js/manifest.js"></script>
@@ -7,3 +30,4 @@
 <script src="assets/js/app.js"></script>
 </body>
 </html>
+<?php include_once 'includes/footer1.php'; ?>
