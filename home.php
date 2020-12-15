0f98aef3a0a0095c81916d8679f5f6c900f5a31f
Victor Grollier <victor.grollier@gmai.com>
2020-12-15 08:58:29 +0100

    home

--- home.php
+++ home.php
@@ -1,9 +1,31 @@
 <?php include_once 'includes/secure.php'; ?>
-<?php include_once 'includes/header.php'; ?>
+<?php include_once 'includes/header.php';
+?>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+
+            </div>
+
+            <div class="col">
+                <div class="card" style="margin-top:100px ;width: 35rem;">
+                    <img src="assets/img/tekkies.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <a href="#" class="btn btn-primary">Like</a>
+                        <h5 class="card-title">Sébastien Nicou</h5>
+                        <p class="card-text">Voici le nouveau logo de TEKKIES</p>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+
+            </div>
 
 
-    <script src="assets/js/manifest.js"></script>
-    <script src="assets/js/vendor.js"></script>
-    <script src="assets/js/app.js"></script>
+            <script src="assets/js/manifest.js"></script>
+            <script src="assets/js/vendor.js"></script>
+            <script src="assets/js/app.js"></script>
 
 <?php include_once 'includes/footer1.php'; ?>
